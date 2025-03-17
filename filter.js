32c732ee367bbef48386c884795903c81dfa21f8
Himanshu Kumar <himankshu1@gmail.com>
2025-03-17 18:43:33 +0530

    rewrite filter.js

--- filter.js
+++ filter.js
@@ -1,18 +1,15 @@
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const numbers = [10, 12, 83, 34, 55, 6, 17, 18, 39];
 
 if (!Array.prototype.myFilter) {
     Array.prototype.myFilter = function (callback) {
         const resultArray = [];
 
         for (let i = 0; i < this.length; i++) {
-            const result = callback(this[i]);
-            if (result) {
-                resultArray.push(this[i]);
-            }
+            callback(this[i]) && resultArray.push(this[i]);
         }
         return resultArray;
     };
 }
 
-const result = numbers.myFilter((val) => val % 2 == 0);
+const result = numbers.myFilter((val) => val % 3 == 0);
 console.log(result);
